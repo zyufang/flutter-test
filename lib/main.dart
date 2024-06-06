ad24897b9ba62d560efd3350e85d5f42bd4d974a
yufang zhao <1625723703@qq.com>
2024-06-06 06:59:43 +0000

    add

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: '赵钰芳 20211060240 Flutter Demo'),
     );
   }
 }
@@ -101,6 +101,9 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
+            ),
+            const Text(
+              '赵钰芳 20211060240'
             ),
           ],
         ),
